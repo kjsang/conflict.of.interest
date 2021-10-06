a7c970f18b3d2786b1ea55c65f874e70e2a1da7c
kjsang <75797388+kjsang@users.noreply.github.com>
2021-10-06 20:04:10 +0900

    1

---
+++ 1.crawling/crawling_conflict.R
@@ -0,0 +1,29 @@
+
+# 패키지 설치 ----------------------------------------------
+
+if (!require("pacman")) (install.packages("pacman"))
+pacman::p_load(
+  tidyverse,
+  rvest, httr
+)
+
+readxl::read_xlsx("bigkinds.xlsx") -> bigkinds
+bigkinds %>% 
+  select(일자, 언론사, 제목, 키워드, URL) %>% 
+  mutate(id = 1:length(제목),
+         date = 일자,
+         press = 언론사,
+         title = 제목,
+         keyword = 키워드,
+         url = URL) %>% 
+  mutate(press = press %>% as.factor()) %>% 
+  select(id, date, press, title, keyword, url) -> base # 언론사를 펙터로 변경
+base %>%
+  filter(press == "중앙일보") %>%
+  select(url) %>% 
+  mutate(text = base$url %>% 
+           read_html() %>% 
+           html_element("div.article_body") %>%
+           html_text2() %>% 
+           as_tibble()
+         )
