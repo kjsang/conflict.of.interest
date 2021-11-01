0c934e073098bced8a2f3652be7c87993c253ed3
kjsang <75797388+kjsang@users.noreply.github.com>
2021-11-01 09:54:36 +0900

    1
    1

--- interest.R
+++ interest.R
@@ -27,7 +27,8 @@ data %>% # 총 3,148 건 데이터 중
            str_replace_all("(오전|오후) [0-9]{1,2}:[0-9]{1,2}", "") %>% # 날짜에 붙은 시간 제거
            lubridate::as_date(), # 날짜변수를 날짜로 정제
          press = press %>%  # 신문사를
-           str_replace_all("\\..*|\n| |(.*| )ⓒ", ""))%>% # . 뒷부분 모두 제거 & \n 태그 제거 & ⓒ 앞부분 모두 제거 
+           str_replace_all("\\..*|\n| |(.*| )ⓒ", ""),
+         ref = paste0("(", press, ", ", date, ")"))%>% # . 뒷부분 모두 제거 & \n 태그 제거 & ⓒ 앞부분 모두 제거 
   filter(press %in% c("경향신문", "국민일보", "내일신문", "동아일보", "문화일보", "서울신문", "세계일보", "조선일보", "중앙일보", "한겨레", "한국일보")) -> data_prep_ver1 # 일간지만 뽑기
 data_prep_ver1 %>% # 일간지만 뽑아서 1,816 건
   mutate(content = content %>%
@@ -45,7 +46,7 @@ data_prep_ver2 %>% write_excel_csv("data_prep_ver2.csv")
 data_prep_ver2 %>% 
   filter(!title %>% str_detect("북한|중국|해외|북핵|외교|국제")) %>% # 1,742 건
   filter(!title %>% str_detect("수상|회고록|In&Out|왜냐면")) %>% # 1,731 건
-  filter(title %>% str_detect("이해충돌|이해관계|공무원|청탁|공직|뇌물|부패|비리|공정|LH|특권|관피아")) -> data_prep_ver3 # 584 건
+  filter(title %>% str_detect("김영란|이해충돌|이해관계|공무원|청탁|공직|뇌물|부패|비리|공정|LH|특권|관피아")) -> data_prep_ver3 # 584 건
 data_prep_ver3 %>% write_excel_csv("data_prep_ver3.csv") # 중간 저장
 
 data_prep_ver2 %>% 
@@ -57,10 +58,11 @@ data_prep_ver2 %>%
 # 2.3. 데이터 정제: 기사제목 기준 필터링 (내용분석) ---------------------
 
 data_prep_ver3 %>% # 584 건 중 내용분석을 통해 걸러내기
-  filter(!title %>% str_detect("특임검사|파워우먼|공직열전|보관신탁|휴직|인재개발")) -> data_prep_ver4
+  filter(!title %>% str_detect("특임검사|파워우먼|공직열전|보관신탁|휴직|인재개발|분쟁조정 심의|파워인터뷰")) -> data_prep_ver4
 data_prep_ver4 # 576 건
 data_prep_ver4 %>% write_excel_csv("data_prep_ver4.csv")
 
+data_prep_ver4 %>% filter(title %>% str_detect("파워인터뷰"))
 
 # 3. 탐색적 분석 -------------------------------------------
 
@@ -95,23 +97,31 @@ data_prep_ver4 %>%
     month = month(date),
     quarter = lubridate::quarter(date),
     yq = paste0(year, ": ", quarter, "Q"),
-    quarterly = lubridate::yq(yq))  %>%
+    quarterly = lubridate::yq(yq))  -> data_prep_ver5 
+
+data_prep_ver5 %>%
   count(quarterly) %>%
   ggplot(aes(x = quarterly, y = n)) +
   geom_line(alpha = 0.8, color = "darkgreen") +
   ylim(0, 150)
 
-# data_prep_ver4 %>% 
-#   mutate(
-#     year = year(date),
-#     month = month(date),
-#     quarter = lubridate::quarter(date),
-#     quarterly = paste0(year, "년_", quarter, "분기")) %>% 
-#   count(quarterly) %>% 
-#   ggplot(aes(x = quarterly, y = n)) +
-#   geom_col() +
-#   theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
-#   scale_x_discrete("Cut")
+data_prep_ver5 %>% 
+  mutate(
+    content = content %>% 
+      str_replace_all("☞.*", "") %>%
+      str_replace_all("▶.*", "") %>%
+      str_replace_all("★.*", "") %>%
+      str_replace_all("이 시각 인기뉴스.*", "") %>% 
+      str_replace_all("\\<.*\\>", "") %>%
+      str_replace_all("◆.*", "") %>% 
+      str_replace_all("\\[(한겨레|내일신문)\\]", "") %>% 
+      str_replace_all("경향신문", "") %>% 
+      str_replace_all("\\[.*\\]", "") %>% 
+      str_replace_all("[a-zA-Z0-9]{1,}@.*", "") %>% 
+      str_replace_all("[가-힣]{2,4} (기자|특파원)", "")
+  ) -> data_prep_ver6
+
+data_prep_ver6 %>% write_excel_csv("data_prep_ver6.csv")
 
            str_replace_all("\\([가-힣]{1,}=[가-힣]{1,}\\)", "") %>% # (서울=연합뉴스) 등 제거
            str_replace_all("[가-힣]{2,4} (기자|특파원)", "") %>% 
@@ -122,16 +132,12 @@ data_prep_ver4 %>%
            str_replace_all("\\[관련기사\\].*", "") %>% 
            str_replace_all("\\[이 시각 많이 본 기사\\].*", "") %>% 
            str_replace_all("\\[뉴시스 이시간 핫 뉴스\\].*|최신 유행 트렌드 총집결.*|기자가 기다려요.*", "") %>% 
-           str_replace_all("☞.*", "") %>%
-           str_replace_all("▶.*", "") %>%
-           str_replace_all("★.*", "") %>%
            str_replace_all("\\(공동취재사진\\)", "") %>%
            str_replace_all("\\[사진 영상 제보받습니다\\].*", "") %>%
            str_replace_all(".*=", "") %>%
            str_replace_all("뉴시스 뉴스, 네이버 뉴스.*", "") %>%
            str_replace_all("[0-9]{4}\\.[0-9]{1,2}\\.[0-9]{1,2}", "") %>%
            str_replace_all("[0-9]{4}\\,[0-9]{1,2}\\,[0-9]{1,2}", "") %>%
-           str_replace_all("\\<.*\\>", "") %>%
            str_replace_all("뉴스1\\)|/(뉴스1|뉴스)|뉴시스】|AP/뉴시스】|뉴시스\\]|AP/뉴시스\\]|뉴시스DB\\)|newiss.com|© News1.*", "") %>%
            str_replace_all("naver.com|nate.com", "") %>%
            str_replace_all("연합뉴스(\\)|\\]| 자료사진\\])", "") %>%
